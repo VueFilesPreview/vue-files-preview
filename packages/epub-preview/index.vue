4738627f36267fe5e225184a6ae9aaf2f6e023e0
SmallTeddy <zhangzeming1212@gmail.com>
2024-07-19 11:34:48 +0800

    fix: epub preview error

--- packages/epub-preview/index.vue
+++ packages/epub-preview/index.vue
@@ -1,29 +1,24 @@
 <template>
-  <div class="epub-box" v-loading="!bookAvailable" element-loading-text="正在加载...">
+  <!-- <div class="epub-box" v-loading="!bookAvailable" element-loading-text="正在加载..."> -->
+  <div class="epub-box">
     <div class="epub-viewer" id="epub-viewer"></div>
     <div class="footer">
-      <el-button type="primary" @click="prevPage">
+      <!-- <el-button type="primary" @click="prevPage">
         上一页
       </el-button>
       <el-button type="primary" @click="nextPage">
         下一页
-      </el-button>
+      </el-button> -->
     </div>
   </div>
 </template>
 
 <script lang='ts' setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import ePub from 'epubjs'
 
 const props = defineProps({
   url: {
-    type: String,
-    default: () => {
-      return 'none'
-    }
-  },
-  type: {
     type: String,
     default: () => {
       return 'none'
@@ -39,52 +34,59 @@ const locations = ref()
 const currentPage = ref(1)
 const totalPages = ref()
 const bookAvailable = ref(false)
-const book = ref(ePub(props.fileRender as ArrayBuffer))
-const rendition = ref(book.value.renderTo("epub-viewer", {
-  // 滚动模式
-  width: '100%',
-  height: 'calc(100vh - 80x)',
-  flow: "scrolled",
-  allowScriptedContent: true,
-}));
+const book = ref()
+const rendition = ref();
 
-book.value.ready.then(() => {
-  navigation.value = book.value.navigation;
-  return book.value.locations.generate(1);
-}).then(() => {
-  locations.value = book.value.locations;
-  bookAvailable.value = true;
-  // 获取总页数
-  totalPages.value = locations.value.length();
-  rendition.value.display();
-});
+const initEpub = () => {
+  if (props.fileRender) {
+    book.value = ePub(props.fileRender as ArrayBuffer)
+    rendition.value = book.value.renderTo("epub-viewer", {
+      // 滚动模式
+      width: '100%',
+      height: 'calc(100vh - 80x)',
+      flow: "scrolled",
+      allowScriptedContent: true,
+    })
+    book.value.ready.then(() => {
+      navigation.value = book.value.navigation;
+      locations.value = book.value.locations;
+      bookAvailable.value = true;
+      // // 获取总页数
+      totalPages.value = locations.value.length();
+      rendition.value.display();
+    })
+  }
+}
+
+watch(
+  () => props.fileRender,
+  () => {
+    initEpub()
+  },
+  { immediate: true }
+)
 
 // epub翻页
-const prevPage = () => {
-  if (rendition.value) {
-    rendition.value.prev();
-    // 向前翻页时更新 currentPage
-    currentPage.value--;
-    if (currentPage.value < 1) {
-      currentPage.value = 1
-    }
-  }
-}
-const nextPage = () => {
-  if (rendition.value) {
-    try {
-      rendition.value.next();
-      // 向后翻页时更新 currentPage
-      currentPage.value++;
-      if (currentPage.value > totalPages.value) {
-        currentPage.value = totalPages.value
-      }
-    } catch (error) {
-      console.log("EPUB 下一页出错：", error);
-      alert("出错，请重试或检查 EPUB 文件格式。");
-    }
-  }
-}
+// const prevPage = () => {
+//   if (rendition.value) {
+//     rendition.value.prev();
+//     // 向前翻页时更新 currentPage
+//     currentPage.value--;
+//     if (currentPage.value < 1) {
+//       currentPage.value = 1
+//     }
+//   }
+// }
+// const nextPage = () => {
+//   if (rendition.value) {
+//     rendition.value.next();
+//     // 向后翻页时更新 currentPage
+//     currentPage.value++;
+//     if (currentPage.value > totalPages.value) {
+//       currentPage.value = totalPages.value
+//     }
+//   }
+// }
 </script>
 
 <style scoped lang='scss'>
