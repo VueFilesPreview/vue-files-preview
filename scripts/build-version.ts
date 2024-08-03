fb80bf59797529f56efad53c462de5bd14564ed3
EternalHeart <hao131462@qq.com>
2024-08-04 03:52:53 +0800

    feat: add version into default

---
+++ scripts/build-version.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env ts-node
+import {writeFileSync, readFileSync} from 'fs';
+import {join} from 'path';
+
+const packageJson = JSON.parse(readFileSync(join(__dirname, "../package.json"), 'utf-8'));
+const versionTsPath = join(__dirname, "../packages", "version.ts");
+const versionTs = `export const version = "${packageJson.version}";`;
+writeFileSync(versionTsPath, versionTs);
+console.log("version.ts create successful.");
